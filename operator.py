f2d7551d8d113e733086fef6a55ab19b428aba20
Vishwan <vishwanv533@gmail.com>
2025-11-11 20:53:11 +0530

    version2

--- operator.py
+++ operator.py
@@ -6,3 +6,9 @@ if amount > 800:
     discount = total * 0.10
     total -= discount
 print (total)
+
+age=18
+if age >= 18:
+    print("you may can vote")
+else:
+    print("you cannot able to vote")
